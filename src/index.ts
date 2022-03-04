2a8b0010e8c3f40201285695a6c092ba24292b3d
Aaron Faanes <dafrito@gmail.com>
2022-03-04 04:10:39 -0600

    Remove artist dependency

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,11 @@
-import NodeValues from "./NodeValues";
-import Artist from "./Artist";
 import NodeRenderData from "./NodeRenderData";
-import Transformed from "./Transformed";
-import Painted from "./Painted";
-import paintGroupBounds from "./paintGroupBounds";
-import ProjectedCaret from "./ProjectedCaret";
-import ProjectedNode, { ProjectedNodeValue } from "./ProjectedNode";
-import WorldTransform from "./WorldTransform";
-
-import Freezable from "./freezer/Freezable";
-import Freezer, { FREEZER_TEXTURE_SCALE } from "./freezer/Freezer";
-import FreezerCache from "./freezer/FreezerCache";
-import FreezerRow, { FREEZER_MARGIN } from "./freezer/FreezerRow";
-import FreezerSlice from "./freezer/FreezerSlice";
-import FreezerSlot from "./freezer/FreezerSlot";
-import FrozenNode from "./freezer/FrozenNode";
-import FrozenNodeFragment from "./freezer/FrozenNodeFragment";
 
 import GraphPainterAnalytics from "./graphpainter/GraphPainterAnalytics";
 import GraphPainter from "./graphpainter/GraphPainter";
 import PaintGroup from "./graphpainter/PaintGroup";
 
 export {
-  NodeValues,
-  Artist,
   NodeRenderData,
-  Transformed,
-  Painted,
-  paintGroupBounds,
-  ProjectedCaret,
-  ProjectedNode,
-  ProjectedNodeValue,
-  WorldTransform,
-  Freezable,
-  FreezerCache,
-  FreezerRow,
-  FREEZER_MARGIN,
-  FreezerSlot,
-  Freezer,
-  FREEZER_TEXTURE_SCALE,
-  FreezerSlice,
-  FrozenNodeFragment,
-  FrozenNode,
   GraphPainterAnalytics,
   GraphPainter,
   PaintGroup,
