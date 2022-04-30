f702a44a85aab33f0c3bdc889230bc2c61ffef67
Aaron Faanes <dafrito@gmail.com>
2022-04-29 22:49:54 -0500

    Remove Viewport export

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import NodeRenderData from "./NodeRenderData";
 import GraphPainterAnalytics from "./graphpainter/GraphPainterAnalytics";
 import GraphPainter from "./graphpainter/GraphPainter";
 import PaintGroup from "./graphpainter/PaintGroup";
-import Viewport from "./Viewport";
 
 export {
   NodeRenderData,
   GraphPainterAnalytics,
   GraphPainter,
-  PaintGroup,
-  Viewport,
+  PaintGroup
 };
