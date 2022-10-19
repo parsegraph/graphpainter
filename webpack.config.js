3bbf00ff040f290480076c4ffb884bfefdda1186
Aaron Faanes <dafrito@gmail.com>
2022-10-19 00:54:59 -0700

    Fix index/lib problem

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
   entry: {
-    lib: relDir("src/index.ts"),
+    index: relDir("src/index.ts"),
     block2d: relDir("src/demo/block2d.ts"),
     block3d: relDir("src/demo/block3d.ts"),
     blockdom: relDir("src/demo/blockdom.ts"),
