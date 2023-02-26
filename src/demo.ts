ea8b277049b2b3175cf833e51419abe10fa41a31
Aaron Faanes <dafrito@gmail.com>
2023-02-25 16:53:48 -0800

    Add basic demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,61 +1,197 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("demo");
-  root.style.position = "relative";
+import Color from "parsegraph-color";
 
-  const container = document.createElement("div");
-  container.innerHTML = "See other demos";
-  container.style.position = "absolute";
-  container.style.left = "0px";
-  container.style.top = "0px";
-  container.style.pointerEvents = "none";
-  root.appendChild(container);
-  container.style.fontSize = "18px";
-  container.style.fontFamily = "sans";
-  const refresh = () => {
-    const rand = () => Math.floor(Math.random() * 255);
-    document.body.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.color = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.left = `${Math.random() * root.clientWidth}px`;
-    container.style.top = `${Math.random() * root.clientHeight}px`;
+import Artist, { PaintedNode } from "parsegraph-artist";
+
+import TimingBelt from "parsegraph-timingbelt";
+import { Projection, BasicProjector, Projector } from "parsegraph-projector";
+import Direction, { DirectionNode } from "parsegraph-direction";
+
+import Viewport from "./Viewport";
+import Block, { BlockNode, style } from "parsegraph-block";
+import { Renderable } from "parsegraph-timingbelt";
+import Method from "parsegraph-method";
+import { showInCamera } from "parsegraph-showincamera";
+import BlockArtist2D from "./demo/BlockArtist2D";
+
+// import Freezer from "../freezer/Freezer";
+
+class DebugOverlay implements Renderable {
+  _onUpdate: Method;
+  _projector: Projector;
+
+  constructor() {
+    this._projector = new BasicProjector();
+    this._onUpdate = new Method();
+
+    this.container().style.pointerEvents = "none";
+    this.container().style.position = "absolute";
+    this.container().style.top = "0px";
+    this.container().style.left = "0px";
+  }
+
+  container() {
+    return this.projector().glProvider().container();
+  }
+
+  tick() {
+    return false;
+  }
+
+  projector() {
+    return this._projector;
+  }
+
+  paint() {
+    this.projector().overlay();
+    return false;
+  }
+
+  width() {
+    return this.projector().width();
+  }
+
+  height() {
+    return this.projector().height();
+  }
+
+  render() {
+    this.projector().render();
+    const ctx = this.projector().overlay();
+    ctx.clearRect(0, 0, this.width(), this.height());
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "top";
+    ctx.fillText("DebugOverlay", 0, 0);
+    ctx.fillStyle = "red";
+    ctx.textAlign = "right";
+    ctx.fillText("DebugOverlay", this.width(), 0);
+    ctx.fillStyle = "blue";
+    ctx.textBaseline = "bottom";
+    ctx.fillText("DebugOverlay", this.width(), this.height());
+    ctx.fillStyle = "yellow";
+    ctx.textAlign = "left";
+    ctx.fillText("DebugOverlay", 0, this.height());
+    return false;
+  }
+
+  unmount() {}
+
+  setOnScheduleUpdate(listener: () => void, listenerObj?: object) {
+    this._onUpdate.set(listener, listenerObj);
+  }
+}
+
+const diagonalBlockDemo = (artistFunc: () => Artist<Block>) => {
+  const makeBlock = (color: Color, borderColor: Color) => {
+    const node = new BlockNode("b");
+    node.value().setBlockStyle({
+      ...node.value().blockStyle(),
+      backgroundColor: color,
+      borderColor,
+    });
+    if (comp) {
+      node.value().setOnScheduleUpdate(() => comp.scheduleUpdate());
+    }
+    return node;
   };
 
-  const dot = document.createElement("div");
-  dot.style.position = "absolute";
-  dot.style.right = "8px";
-  dot.style.top = "8px";
-  dot.style.width = "16px";
-  dot.style.height = "16px";
-  dot.style.borderRadius = "8px";
-  dot.style.transition = "background-color 400ms";
-  dot.style.backgroundColor = "#222";
-  root.appendChild(dot);
+  const belt = new TimingBelt();
 
-  container.style.transition = "color 2s, left 2s, top 2s";
-  document.body.style.transition = "background-color 2s";
-  let timer: any = null;
-  let dotTimer: any = null;
-  let dotIndex = 0;
-  const dotState = ["#f00", "#c00"];
-  const refreshDot = () => {
-    dotIndex = (dotIndex + 1) % dotState.length;
-    dot.style.backgroundColor = dotState[dotIndex];
-  };
-  const interval = 3000;
-  const dotInterval = 500;
-  root.addEventListener("click", () => {
-    if (timer) {
-      clearInterval(timer);
-      timer = null;
-      clearInterval(dotTimer);
-      dotTimer = null;
-      dot.style.transition = "background-color 3s";
-      dot.style.backgroundColor = "#222";
-    } else {
-      refresh();
-      dot.style.transition = "background-color 400ms";
-      refreshDot();
-      timer = setInterval(refresh, interval);
-      dotTimer = setInterval(refreshDot, dotInterval);
+  let rootNode = makeBlock(new Color(1, 1, 1), new Color(0.5, 0.5, 0.5, 0.5));
+  const comp = new Viewport(rootNode);
+  rootNode.value().setOnScheduleUpdate(() => comp.scheduleUpdate());
+  // const freezer = new Freezer();
+  // root.value().getCache().freeze(freezer);
+
+  for (let i = 0; i < 1; ++i) {
+    let n: PaintedNode = rootNode;
+    for (let j = 0; j < 10; ++j) {
+      const child = makeBlock(
+        new Color(1 - i / 10, 0, 0),
+        new Color(0.5, 0.5, 0.5, 0.5)
+      );
+      child.connectNode(
+        Direction.INWARD,
+        makeBlock(new Color(1 - i / 10, 1, 0), new Color(0.5, 0.5, 0.5, 0.5))
+      );
+      child.nodeAt(Direction.INWARD).crease();
+      if (Math.random() < 0.5) {
+        child.state().setScale(0.85);
+      }
+      n.connectNode(j % 2 ? Direction.DOWNWARD : Direction.FORWARD, child);
+      n = child;
+      if (j % 5 == 0) {
+        n.crease();
+        n.value().setLabel(n.id());
+      }
+    }
+    rootNode.connectNode(
+      Direction.UPWARD,
+      makeBlock(new Color(1, 1, 1, 1), new Color(1, 0, 0, 1))
+    );
+    rootNode = rootNode.nodeAt(Direction.UPWARD);
+  }
+
+  console.log(
+    "PGs: ",
+    comp
+      .root()
+      .paintGroup()
+      .dump()
+      .map((pg) => pg.id())
+      .join(", ")
+  );
+
+  window.addEventListener("resize", () => {
+    belt.scheduleUpdate();
+  });
+
+  const topElem = document.getElementById("demo");
+
+  const projector = new BasicProjector();
+  topElem.appendChild(projector.container());
+  projector.container().style.position = "absolute";
+  const proj = new Projection(projector, comp);
+  belt.addRenderable(proj);
+  const debugOverlay = new DebugOverlay();
+  topElem.appendChild(debugOverlay.container());
+  debugOverlay.container().style.position = "absolute";
+  debugOverlay.container().style.pointerEvents = "none";
+  belt.addRenderable(debugOverlay);
+  setTimeout(() => {
+    showInCamera(comp.root(), comp.camera(), false);
+  }, 100);
+
+  const root = projector.container();
+  root.tabIndex = 0;
+  let clicked = false;
+  root.addEventListener("mousedown", (e) => {
+    if (e.button === 0) {
+      clicked = true;
     }
   });
+  root.addEventListener("mousemove", (e) => {
+    if (!clicked) {
+      return;
+    }
+    const cam = comp.camera();
+    cam.adjustOrigin(e.movementX / cam.scale(), e.movementY / cam.scale());
+    comp.scheduleRepaint();
+    belt.scheduleUpdate();
+  });
+  root.addEventListener("mouseup", (e) => {
+    if (e.button === 0) {
+      clicked = false;
+    }
+  });
+  root.addEventListener("wheel", (e) => {
+    const zoomIn = (e as WheelEvent).deltaY < 0;
+    console.log(e);
+    comp.camera().zoomToPoint(zoomIn ? 1.1 : 0.9, e.clientX, e.clientY);
+    comp.scheduleRepaint();
+    belt.scheduleUpdate();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  diagonalBlockDemo(() => BlockArtist2D.instance());
 });
